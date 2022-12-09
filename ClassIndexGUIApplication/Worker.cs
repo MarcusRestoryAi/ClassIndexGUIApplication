0d8751b327667f8dbc9a6fc359808692df3d14e3
Marcus <marcus.henriksson@restory.ai>
2022-12-09 09:25:47 +0100

    Worker Class Created

---
+++ ClassIndexGUIApplication/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIndexGUIApplication
+{
+    internal class Worker : Person
+    {
+        public string WorkTitle { get; set; }
+        public Worker(string name, int age, string workTitle) : base(name, age)
+        {
+            WorkTitle = workTitle;
+        }
+    }
+}
